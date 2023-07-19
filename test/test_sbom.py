ee23128bce1c1c0cdb2cfd1d6c2713bedaee3210
Frantisek Hrbata <frantisek.hrbata@espressif.com>
2023-07-19 17:53:18 +0200

    ci: initial tests
    This addes some basic tests for generating sbom, checking sbom for
    vulnerabilities and spdx validation.
    
    Signed-off-by: Frantisek Hrbata <frantisek.hrbata@espressif.com>

---
+++ test/test_sbom.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+from distutils.dir_util import copy_tree
+from pathlib import Path
+from subprocess import run
+from tempfile import TemporaryDirectory
+from textwrap import dedent
+
+import pytest
+
+
+@pytest.fixture
+def hello_world_build(ctx: dict={'tmpdir': None}) -> Path:
+    # build hello_world app in temporary directory and return its path
+    if ctx['tmpdir']:
+        return Path(ctx['tmpdir'].name)
+
+    tmpdir = TemporaryDirectory()
+    hello_world_path = Path(os.environ['IDF_PATH']) / 'examples' / 'get-started' / 'hello_world'
+    copy_tree(str(hello_world_path), tmpdir.name, verbose=0)
+    run(['idf.py', 'fullclean'], cwd=tmpdir.name, check=True)
+    run(['idf.py', 'build'], cwd=tmpdir.name, check=True)
+    ctx['tmpdir'] = tmpdir
+    return Path(tmpdir.name)
+
+
+def test_generate_sbom(hello_world_build: Path) -> None:
+    proj_desc_path = hello_world_build / 'build' / 'project_description.json'
+    run([sys.executable, '-m', 'esp_idf_sbom', 'create', proj_desc_path], check=True)
+
+
+def test_check_sbom(hello_world_build: Path) -> None:
+    tmpdir = TemporaryDirectory()
+    output_fn = Path(tmpdir.name) / 'sbom.spdx'
+    proj_desc_path = hello_world_build / 'build' / 'project_description.json'
+    run([sys.executable, '-m', 'esp_idf_sbom', 'create', '-o', output_fn, proj_desc_path], check=True)
+    run([sys.executable, '-m', 'esp_idf_sbom', '-v', 'check', output_fn], check=True)
+
+
+def test_sbom_project_manifest(hello_world_build: Path) -> None:
+    manifest = hello_world_build / 'sbom.yml'
+    content = '''
+              version: 999.999.999
+              description: testing hello_world application
+              url: https://test.hello.world.org/hello_world-0.1.0.tar.gz
+              cpe: cpe:2.3:a:hello_world:hello_world:{}:*:*:*:*:*:*:*
+              supplier: 'Person: John Doe'
+              '''
+    manifest.write_text(dedent(content))
+    proj_desc_path = hello_world_build / 'build' / 'project_description.json'
+    p = run([sys.executable, '-m', 'esp_idf_sbom', 'create', proj_desc_path],
+            check=True, capture_output=True, text=True)
+
+    assert 'PackageVersion: 999.999.999' in p.stdout
+    assert 'PackageSummary: <text>testing hello_world application</text>' in p.stdout
+    assert 'PackageDownloadLocation: https://test.hello.world.org/hello_world-0.1.0.tar.gz' in p.stdout
+    assert 'ExternalRef: SECURITY cpe23Type cpe:2.3:a:hello_world:hello_world:999.999.999:*:*:*:*:*:*:*' in p.stdout
+    assert 'PackageSupplier: Person: John Doe' in p.stdout
+
+    manifest.unlink()
+
+
+def test_validate_sbom(hello_world_build: Path) -> None:
+    tmpdir = TemporaryDirectory()
+    output_fn = Path(tmpdir.name) / 'sbom.spdx'
+    proj_desc_path = hello_world_build / 'build' / 'project_description.json'
+    run([sys.executable, '-m', 'esp_idf_sbom', 'create', '--files', 'rem',
+         '-o', output_fn, proj_desc_path],
+        check=True)
+    run(['pyspdxtools', '-i', output_fn], check=True)
