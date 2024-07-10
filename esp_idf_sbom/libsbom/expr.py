espressif/esp-idf-sbom

commit bdcacd2b2aba12193607a49646544637b513da91

feat: allow conditional expressions in manifest files
Enable the use of expressions to conditionally include packages in the
SBOM. A new "if" key has been added to the manifest file, allowing
expressions based on configuration options. If the expression evaluates
to true, the package is included in the SBOM; otherwise, it is skipped.
The "if" key is ignored for project and component packages, as they must
always be included to ensure the SBOM is complete.

The expression evaluation is influenced by kconfig and supports the
following operators in descending order of precedence, which can be
modified using brackets.

- !: negation
- <, >, >=, <=: comparison
- =, !=: equal, not equal
- &&: logical and
- ||: logical or

Examples:
*  if: 'IDF_TARGET = "esp32"'
*  if: '(IDF_TARGET_ESP32 || IDF_TARGET = "esp32") && !IDF_TOOLCHAIN_GCC'
*  if: '1536 <= FREERTOS_ISR_STACKSIZE < 0xffff'

Signed-off-by: Frantisek Hrbata <frantisek.hrbata@espressif.com>

esp_idf_sbom/libsbom/expr.py
@@ -1,0 +1,124 @@
+# SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Basic evaluation of expressions for configuration values specified with kconfig.
+"""
+
+from typing import Any, Dict, Union
+
+import pyparsing as pp
+
+# Parse actions used should not have any side effects, ensuring
+# the safety of using packrat. Parsing expressions with brackets
+# is very slow without caching.
+pp.ParserElement.enable_packrat()
+
+ppc = pp.pyparsing_common
+
+
+def _boolit(operator: Union[bool, int, str]) -> bool:
+    # kconfig-language.txt:
+    #   Boolean and tristate symbols are simply converted into
+    #   the respective expression values. All other symbol types
+    #   result in 'n'.
+    # This implementation uses only a boolean, not a tristate.
+    return False if type(operator) is not bool else operator
+
+
+def _eval_binary_operator(toks: pp.ParseResults, logical: bool=False) -> bool:
+    operator_map = {
+        '=': lambda o1, o2: o1 == o2,
+        '!=': lambda o1, o2: o1 != o2,
+        '<': lambda o1, o2: o1 < o2,
+        '>': lambda o1, o2: o1 > o2,
+        '<=': lambda o1, o2: o1 <= o2,
+        '>=': lambda o1, o2: o1 >= o2,
+        '&&': lambda o1, o2: o1 and o2,
+        '||': lambda o1, o2: o1 or o2,
+    }
+
+    tokens = toks[0]
+    operand1 = _boolit(tokens.pop(0)) if logical else tokens.pop(0)
+    while len(tokens):
+        # Evaluate all operators that have the same precedence level in this loop
+        # The tokens may include, for example, ['VAR1', '>', 'VAR2', '>=', 'VAR3', '<', 'VAR4'].
+        operator = tokens.pop(0)
+        operand2 = _boolit(tokens.pop(0)) if logical else tokens.pop(0)
+        if type(operand1) is not type(operand2):
+            # No magic comparison. If the types don't match, just bail out.
+            # Keep in mind that operands for logical operators will always
+            # be boolean, so this situation should not occur for them.
+            return False
+        res = operator_map[operator](operand1, operand2)
+        # Short-circuit evaluation
+        if operator == '||' and res:
+            return True
+        if operator != '||' and not res:
+            return False
+
+        operand1 = operand2
+
+    if operator == '||':
+        return False
+
+    return True
+
+
+def _eval_comparison_operator(toks: pp.ParseResults) -> bool:
+    return _eval_binary_operator(toks)
+
+
+def _eval_logical_operator(toks: pp.ParseResults) -> bool:
+    return _eval_binary_operator(toks, logical=True)
+
+
+def _eval_unary_operator(toks: pp.ParseResults) -> bool:
+    return not _boolit(toks[0][1])
+
+
+def _eval_variable(toks: pp.ParseResults):
+    # Return the variable value from sdkconfig.json or from predefined boolean variables.
+    # If the variable is not found, return False by default.
+    global _variables
+    variable = toks[0]
+
+    default_variables = {
+        'true': True,
+        'True': True,
+        'false': False,
+        'False': False,
+    }
+
+    return _variables.get(variable, default_variables.get(variable, False))
+
+
+_variables: Dict[str, Any] = {}
+_variable = ppc.identifier.set_parse_action(_eval_variable)
+_hex = pp.Regex(r'0[xX][0-9a-fA-F]+').set_parse_action(lambda t: int(t[0][2:], 16))
+_number = ppc.number
+_string = pp.QuotedString('"', '\\')
+
+_expr = pp.infix_notation(
+    _variable | _hex | _number | _string,
+    [
+        # Precedence according to Linux bison scripts/kconfig/parser.y
+        ('!', 1, pp.OpAssoc.RIGHT, _eval_unary_operator),
+        (pp.one_of('< > >= <='), 2, pp.OpAssoc.LEFT, _eval_comparison_operator),
+        (pp.one_of('= !='), 2, pp.OpAssoc.LEFT, _eval_comparison_operator),
+        ('&&', 2, pp.OpAssoc.LEFT, _eval_logical_operator),
+        ('||', 2, pp.OpAssoc.LEFT, _eval_logical_operator),
+    ])
+
+
+def set_variables(variables: Dict[str, Any]) -> None:
+    global _variables
+    _variables = variables
+
+
+def evaluate(string: str) -> bool:
+    try:
+        res = _expr.parse_string(string, parse_all=True)
+    except pp.exceptions.ParseException as e:
+        raise RuntimeError(e)
+    return _boolit(res[0])

setup.py
@@ -49,6 +49,7 @@ setup(
         'schema',
         'license-expression',
         'rich',
+        'pyparsing',
     ],
     extras_require={
         'dev': [

esp_idf_sbom/libsbom/mft.py
@@ -10,7 +10,7 @@ import schema
 import yaml
 from license_expression import ExpressionError, get_spdx_licensing
 
-from esp_idf_sbom.libsbom import git, log, utils
+from esp_idf_sbom.libsbom import expr, git, log, utils
 
 licensing = get_spdx_licensing()
 
@@ -269,6 +269,13 @@ def validate(manifest: Dict[str,str], source:str, directory:str, die:bool=True) 
             check_manifest_path(pkg)
         return True
 
+    def check_if(expression: str) -> bool:
+        try:
+            expr.evaluate(expression)
+        except RuntimeError as e:
+            raise schema.SchemaError((f'Expression "{expression}" is not valid: {e}'))
+        return True
+
     cve_exclude_list_schema = schema.Schema(
         [{
             'cve': str,
@@ -297,6 +304,7 @@ def validate(manifest: Dict[str,str], source:str, directory:str, die:bool=True) 
             schema.Optional('cve-exclude-list'): cve_exclude_list_schema,
             schema.Optional('manifests'): manifests_schema,
             schema.Optional('virtpackages'): schema.And(list, check_virtpackages),
+            schema.Optional('if'): schema.And(str, check_if),
         }, ignore_extra_keys=True)
 
     try:

esp_idf_sbom/libsbom/spdx.py
@@ -20,7 +20,7 @@ import yaml
 from license_expression import ExpressionError, get_spdx_licensing
 
 from esp_idf_sbom import __version__
-from esp_idf_sbom.libsbom import git, log, mft, utils
+from esp_idf_sbom.libsbom import expr, git, log, mft, utils
 
 
 class SPDXTags:
@@ -259,6 +259,7 @@ class SPDXObject:
         'cve-exclude-list': [],
         'manifests': [],
         'virtpackages': [],
+        'if': '',
     }
 
     # Global dictionary with manifest files referenced in sbom.yml or idf_component.yml
@@ -442,6 +443,7 @@ class SPDXDocument(SPDXObject):
     """Main SPDX Creation Information"""
     def __init__(self, args: Namespace, proj_desc_path: str):
         proj_desc = self._get_proj_desc(proj_desc_path)
+        self._set_expr_variables(proj_desc, args)
 
         super().__init__(args, proj_desc)
 
@@ -477,6 +479,18 @@ class SPDXDocument(SPDXObject):
 
         return proj_desc  # type: ignore
 
+    def _set_expr_variables(self, proj_desc: Dict[str, Any], args: Namespace) -> None:
+        sdkconfig_path = utils.pjoin(proj_desc['build_dir'], 'config', 'sdkconfig.json')
+        try:
+            with open(sdkconfig_path, 'r') as f:
+                sdkconfig = json.load(f)
+        except (OSError, ValueError) as e:
+            log.warn((f'Unable to read configuration variables from the sdkconfig JSON file: {e}. '
+                      'Conditional statements in manifest files will not be considered.'))
+            args.disable_conditions = True
+        else:
+            expr.set_variables(sdkconfig)
+
     def dump(self) -> str:
         header = ' '.join('\"' + arg + '\"' if ' ' in arg else arg for arg in sys.argv)
         out = '[blue]'
@@ -506,6 +520,9 @@ class SPDXPackage(SPDXObject):
                        project contains only the final bin file.
     get_tags:          Create SPDXTags object for package. For example project gathers tags from
                        components and thier subpackages/submodules.
+    include_package:   Return True if the package should be included in the SBOM. This is used for
+                       evaluating "if" expressions in manifest files. Certain packages, such as project
+                       and component packages, cannot be excluded.
     dump:              Print package SPDX representation.
     """
 
@@ -515,12 +532,19 @@ class SPDXPackage(SPDXObject):
         self.name = name
         self.mark = mark
         self.dir = path
+        self.include = True
+        self.args = args
 
         self.subpackages: List['SPDXPackage'] = []
         self.files: List['SPDXFile'] = []
         self.tags: SPDXTags = SPDXTags()
 
         self.manifest = self.get_manifest(self.dir)
+
+        if not self.include_package():
+            self.include = False
+            return
+
         # CPEs may contain version placeholder.
         self.manifest['cpe'] = [cpe.format(self.manifest['version']) for cpe in self.manifest['cpe']]
 
@@ -597,6 +621,14 @@ class SPDXPackage(SPDXObject):
 
         self.add_relationships()
 
+    def include_package(self) -> bool:
+        if self.args.disable_conditions:
+            # Expressions disregarded due to the --disable-conditions command line option.
+            return True
+        if not self.manifest['if']:
+            return True
+        return expr.evaluate(self.manifest['if'])
+
     def add_relationships(self):
         for subpkg in self.subpackages:
             self['Relationship'] += [f'{self["SPDXID"][0]} DEPENDS_ON {subpkg["SPDXID"][0]}']
@@ -621,10 +653,14 @@ class SPDXPackage(SPDXObject):
         if self.args.rem_submodules and self.args.rem_subpackages:
             return subpackages
 
+        pkg: Optional[SPDXPackage] = None
+
         for virtpkg in self.manifest['virtpackages']:
             fullpath = utils.pjoin(self.dir, virtpkg)
             name = '{}-{}'.format(self.name, utils.prelpath(fullpath, self.dir))
-            subpackages.append(SPDXVirtpackage(self.args, self.proj_desc, fullpath, name))
+            pkg = SPDXVirtpackage(self.args, self.proj_desc, fullpath, name)
+            if pkg.include:
+                subpackages.append(pkg)
 
         submodules_info: List[Dict[str,str]] = []
         if not self.args.rem_submodules:
@@ -635,15 +671,19 @@ class SPDXPackage(SPDXObject):
         submodules_info_dict = {i['path']:i for i in submodules_info}
 
         for root, dirs, files in utils.pwalk(self.dir, [self.dir]):
+            pkg = None
             if not self.args.rem_subpackages and root in submodules_info_dict:
                 submodule_info = submodules_info_dict[root]
                 name = '{}-{}'.format(self.name, utils.prelpath(submodule_info['path'], self.dir))
-                subpackages.append(SPDXSubmodule(self.args, self.proj_desc, name, submodule_info))
+                pkg = SPDXSubmodule(self.args, self.proj_desc, name, submodule_info)
                 dirs.clear()
             elif not self.args.rem_subpackages and ('sbom.yml' in files or root in self.REFERENCED_MANIFESTS):
                 name = '{}-{}'.format(self.name, utils.prelpath(root, self.dir))
-                subpackages.append(SPDXSubpackage(self.args, self.proj_desc, root, name))
+                pkg = SPDXSubpackage(self.args, self.proj_desc, root, name)
                 dirs.clear()
+
+            if pkg is not None and pkg.include:
+                subpackages.append(pkg)
 
         return subpackages
 
@@ -814,6 +854,12 @@ class SPDXProject(SPDXPackage):
 
         return components
 
+    def include_package(self):
+        if self.manifest['if']:
+            log.warn((f'The manifest file for the project "{self.dir}" includes an "if" expression '
+                      f'that will be disregarded. The project package cannot be excluded.'))
+        return True
+
     def get_files(self, path: str, prefix: str, exclude_dirs: Optional[List[str]]=None) -> List['SPDXFile']:
         # project has just the final binary file
         if not self.include_files():
@@ -914,6 +960,10 @@ class SPDXToolchain(SPDXPackage):
         manifest['supplier'] = self.ESPRESSIF_SUPPLIER
         return manifest
 
+    def include_package(self):
+        # The toolchain package cannot be excluded using an "if" expression in the manifest file.
+        return True
+
     def get_subpackages(self):
         # There are not subpackages for toolchain
         return []
@@ -979,6 +1029,12 @@ class SPDXComponent(SPDXPackage):
     def __init__(self, args: Namespace, proj_desc: Dict[str, Any], name: str, info: dict):
         self.info = info
         super().__init__(args, proj_desc, info['dir'], name, 'component')
+
+    def include_package(self):
+        if self.manifest['if']:
+            log.warn((f'The manifest file for the component "{self.dir}" includes an "if" expression '
+                      f'that will be disregarded. The component package cannot be excluded.'))
+        return True
 
 
 class SPDXVirtpackage(SPDXPackage):

esp_idf_sbom/sbom.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
@@ -558,6 +558,9 @@ def main():
                                default=bool(os.environ.get('SBOM_CREATE_NO_FILE_TAGS')),
                                help=('Scan files for SPDX file tags. This includes SPDX-License-Identifier, '
                                      'SPDX-FileCopyrightText and SPDX-FileContributor'))
+    create_parser.add_argument('--disable-conditions',
+                               action='store_true',
+                               help=('When processing manifest files, disregard the conditions for the "if" key.'))
 
     check_parser = subparsers.add_parser('check',
                                          help=('Check components/submodules in the ESP-IDF SBOM file '
@@ -627,6 +630,10 @@ def main():
                                 default=bool(os.environ.get('SBOM_LICENSE_UNIFY_COPYRIGHTS')),
                                 help=('Unify copyright years. If the same copyright is used at different '
                                       'places with different years or year ranges, this option will unify them.'))
+
+    license_parser.add_argument('--disable-conditions',
+                                action='store_true',
+                                help=('When processing manifest files, disregard the conditions for the "if" key.'))
 
     manifest_parser = subparsers.add_parser('manifest',
                                             help=('Commands operating atop of manifest files.'))

test/test_sbom.py
@@ -299,3 +299,63 @@ def test_virtual_package(hello_world_build: Path) -> None:
     manifest.unlink()
     virtpackage.unlink()
     return
+
+
+def test_manifest_expression(hello_world_build: Path) -> None:
+    """Add a virtual package with several different "if" expressions and check whether it is included."""
+    manifest = hello_world_build / 'main' / 'sbom.yml'
+    virtpackage = hello_world_build / 'main' / 'virtpackage.yml'
+    proj_desc_path = hello_world_build / 'build' / 'project_description.json'
+
+    content = f'''
+              virtpackages:
+                - virtpackage.yml
+              '''
+
+    manifest.write_text(dedent(content))
+
+    # Should be included
+    content = f'''
+              name: EXPR_VIRTUAL_PACKAGE
+              if: 'IDF_TARGET = "esp32" && !!!!IDF_TARGET_ESP32 && LOG_DEFAULT_LEVEL > 1'
+              '''
+    virtpackage.write_text(dedent(content))
+
+    p = run([sys.executable, '-m', 'esp_idf_sbom', 'create', proj_desc_path],
+            check=True, capture_output=True, text=True)
+
+    assert 'EXPR_VIRTUAL_PACKAGE' in p.stdout
+
+    # Should be included
+    content = f'''
+              name: EXPR_VIRTUAL_PACKAGE
+              if: 'IDF_TARGET_ESP32S3 || (IDF_TARGET = "esp32" && IDF_TARGET_ARCH_XTENSA = True)'
+              '''
+    virtpackage.write_text(dedent(content))
+
+    p = run([sys.executable, '-m', 'esp_idf_sbom', 'create', proj_desc_path],
+            check=True, capture_output=True, text=True)
+
+    assert 'EXPR_VIRTUAL_PACKAGE' in p.stdout
+
+    # Should NOT be included
+    content = f'''
+              name: EXPR_VIRTUAL_PACKAGE
+              if: 'IDF_TARGET_ESP32S3 || !IDF_TARGET_ARCH_XTENSA'
+              '''
+    virtpackage.write_text(dedent(content))
+
+    p = run([sys.executable, '-m', 'esp_idf_sbom', 'create', proj_desc_path],
+            check=True, capture_output=True, text=True)
+
+    assert 'EXPR_VIRTUAL_PACKAGE' not in p.stdout
+
+    # Should be included because the --disable-conditions is used
+    p = run([sys.executable, '-m', 'esp_idf_sbom', 'create', '--disable-conditions', proj_desc_path],
+            check=True, capture_output=True, text=True)
+
+    assert 'EXPR_VIRTUAL_PACKAGE' in p.stdout
+
+    manifest.unlink()
+    virtpackage.unlink()
+    return

commit 89a64c1febd778ed96cf2745ab63fb06f6998f53

fix: ensure pyparsing usage remains compatible with version 2.2.2 or newer
Commit bdcacd2b2aba ("feat: allow conditional expressions in manifest
files") introduced a requirement for pyparsing. Unfortunately, the API
used in expr.py is not backward compatible with older versions of
pyparsing. This update resolves the issue by using the older camel case
API, allowing compatibility with pyparsing versions starting from 2.2.2.
This change also ensures compatibility with esp-idf v4.4, which requires
'pyparsing>=2.0.3,<2.4.0'. The minimum version of 2.2.2 is chosen
because it works with newer Python versions (3.10+), where
MutableMapping was removed from the collections module. Consequently,
pyparsing versions prior to 2.2.2 do not work with Python 3.10+. Refer
to cab6d8980163 ("Fix collections ABCs deprecation warning") in
the pyparsing repository for more details.

Fixes: bdcacd2b2aba ("feat: allow conditional expressions in manifest files")
Signed-off-by: Frantisek Hrbata <frantisek.hrbata@espressif.com>

esp_idf_sbom/libsbom/expr.py
@@ -12,7 +12,7 @@ import pyparsing as pp
 # Parse actions used should not have any side effects, ensuring
 # the safety of using packrat. Parsing expressions with brackets
 # is very slow without caching.
-pp.ParserElement.enable_packrat()
+pp.ParserElement.enablePackrat()
 
 ppc = pp.pyparsing_common
 
@@ -94,20 +94,20 @@ def _eval_variable(toks: pp.ParseResults):
 
 
 _variables: Dict[str, Any] = {}
-_variable = ppc.identifier.set_parse_action(_eval_variable)
-_hex = pp.Regex(r'0[xX][0-9a-fA-F]+').set_parse_action(lambda t: int(t[0][2:], 16))
+_variable = ppc.identifier.setParseAction(_eval_variable)
+_hex = pp.Regex(r'0[xX][0-9a-fA-F]+').setParseAction(lambda t: int(t[0][2:], 16))
 _number = ppc.number
 _string = pp.QuotedString('"', '\\')
 
-_expr = pp.infix_notation(
+_expr = pp.infixNotation(
     _variable | _hex | _number | _string,
     [
         # Precedence according to Linux bison scripts/kconfig/parser.y
-        ('!', 1, pp.OpAssoc.RIGHT, _eval_unary_operator),
-        (pp.one_of('< > >= <='), 2, pp.OpAssoc.LEFT, _eval_comparison_operator),
-        (pp.one_of('= !='), 2, pp.OpAssoc.LEFT, _eval_comparison_operator),
-        ('&&', 2, pp.OpAssoc.LEFT, _eval_logical_operator),
-        ('||', 2, pp.OpAssoc.LEFT, _eval_logical_operator),
+        ('!', 1, pp.opAssoc.RIGHT, _eval_unary_operator),
+        (pp.oneOf('< > >= <='), 2, pp.opAssoc.LEFT, _eval_comparison_operator),
+        (pp.oneOf('= !='), 2, pp.opAssoc.LEFT, _eval_comparison_operator),
+        ('&&', 2, pp.opAssoc.LEFT, _eval_logical_operator),
+        ('||', 2, pp.opAssoc.LEFT, _eval_logical_operator),
     ])
 
 

setup.py
@@ -49,7 +49,7 @@ setup(
         'schema',
         'license-expression',
         'rich',
-        'pyparsing',
+        'pyparsing>=2.2.2',
     ],
     extras_require={
         'dev': [